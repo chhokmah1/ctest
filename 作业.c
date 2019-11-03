40b984d4c5d0722727cef9ed73599656bd27b0d0
chhokmah1 <56283299+chhokmah1@users.noreply.github.com>
2019-11-03 16:32:23 +0800

    Add files via upload

--- 作业.c
+++ 作业.c
@@ -1,34 +1,20 @@
 #include <stdio.h>
 int main()
 {
-  int a, b, c, d, sum;
-  double average;
-  printf("enter number");
-  scanf("%d%d%d%d", &a, &b, &c, &d);
-  sum = a + b + c + d;
-  average = sum / 4;
-  printf("sum=%d\naverage=%.1f", sum, average);
+  int electricit;
+  float cost;
+  printf("enter your electricit=");
+  scanf("%d",&electricit);
+  if(electricit<=50)
+  {
+    cost=0.53*electricit;
+  }
+  else
+  {
+    cost=0.53*50+0.25*(cost-50);
+  }
+  
+  printf("your cost is %.2fyuan\n",cost);
 
   return 0;
 }
-
-//n个数求和//
-
-// #include <stdio.h>
-// int main()
-// {
-//   int n, sum = 0, number, i;
-//   double average;
-//   printf("enter n=");
-//   scanf("%d", &n);
-//   i = n;
-//   for (; n > 0; n--)
-//   {
-//     printf("enter number=");
-//     scanf("%d", &number);
-//     sum += number;
-//   }
-//   average = sum / i;
-//   printf("sum =%d\naverage=%.1f\n", sum, average);
-//   return 0;
-// }
