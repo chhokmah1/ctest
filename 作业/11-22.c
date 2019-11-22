a29c3cda15be2a08efa318b5273a048db18fdc33
chhokmah1 <56283299+chhokmah1@users.noreply.github.com>
2019-11-22 15:42:08 +0800

    Add files via upload

---
+++ 作业/11-22.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    double chick;
+   int cock ,hen ,x ,y ,z;
+   for (cock=0;cock*5<=100;cock++)
+   {
+       for (hen=0;hen*3<=100;hen++)
+       {
+           for(chick=0;chick/3.0<=100;chick++)
+           {
+               if (cock*5+hen*3+chick/3.0==100&&cock+hen+chick==100)
+               {
+                   printf("cock=%d hen=%d chick=%.0f\n",cock,hen,chick);                  
+               }
+               
+           }
+
+       } 
+   }
+   return 0;
+}
