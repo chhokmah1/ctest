e1873c7525618308423f163cb1af9ccd2336dc9f
chhokmah1 <56283299+chhokmah1@users.noreply.github.com>
2019-11-29 08:28:09 +0800

    Update 11-26.c

--- 作业/11月/11-26.c
+++ 作业/11月/11-26.c
@@ -2,22 +2,24 @@
 #include<math.h>
 int main()
 {
-    int i,j,k;
-    for ( i = 2; i <=500; i++)
+    int i,j,k,i1=0;
+    for ( i = 2; i <=100; i++)
     {
         k=(int)sqrt( i) ;
-    for(j=2;j<=k;j++)
-        {
-            if(i%j==0){
-            break;
-            }
+    for(j=2;j<=k;j++){
+            if(i%j==0)
+            break;       
         }
 
-         if(j>k)
-        {
-            printf("%d ",i);
-        }
-    
+         if(j>k){
+            printf("%4d ",i);
+            i1++;
+            if(i1%10==0){
+             printf("\n");
+            }
+         }
     }
+
     return 0;
 }
+
